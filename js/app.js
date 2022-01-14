4c2fc78db75d4484558465f86b316c1795e5653b
Matt Rangel <mattrangel@gmail.com>
2022-01-14 14:01:43 -0800

    Format first problem correctly

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-   let sum = a + b;
+  let sum = a + b;
 
- return [sum,`The sum of ${a} and ${b} is ${sum}.`];
+  return [sum,`The sum of ${a} and ${b} is ${sum}.`];
 
 }
 
